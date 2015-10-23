370ab9cf67039fdc404feb47fd630e329ebafeb4
Ryan Liptak <squeek502@hotmail.com>
2015-10-23 16:59:46 -0700

    Expand filters much more  * Add adverb matching  * Add some more phrase variants

--- filters/english.js
+++ filters/english.js
@@ -1,4 +1,14 @@
+var adverbs = [
+	'really', 'totally', 'probably', 'defin[ia]tely', 'absolutely', 'actually',
+	'certainly', 'literally', 'legitimately', 'genuinely', 'honestly', 'truly',
+	'undoubtedly', 'unquestionably'
+];
+var adverbsRegexSet = adverbs.join('|');
+
 module.exports = [
-    /i (want to|wanna|would like to) (be|become|go)( a)? vegan/i,
-    /help me (be|become|go)( a)? vegan/i
+    /help me (be( a)?|become( a)?|go) vegan/i,
+    new RegExp("i (" + adverbsRegexSet + ")? ?(want to|wanna|would like to|should) (be( a)?|become( a)?|go) vegan", "i"),
+    new RegExp("i (" + adverbsRegexSet + ")? ?(will|do)? ?(need|want) help (going|becoming( a)?|being( a)?|staying( a)?) vegan", "i"),
+    new RegExp("i (" + adverbsRegexSet + ")? ?(want to|wanna|would like to|should)? ?(try) (going|becoming( a)?|being( a)?) vegan", "i"),
+    /i('| a)?m (considering|thinking (about|of)|mulling over) (going|becoming( a)?|being( a)?) vegan/i
 ]
--- test/test-filter.js
+++ test/test-filter.js
@@ -14,13 +14,33 @@ var matches = [
     "I want to be a vegan",
     "I wanna become vegan",
     "I wanna be a vegan",
+    "I really wanna be vegan",
+    "I should go vegan",
+    "I probably should go vegan",
+    "I need help going vegan",
+    "I want help going vegan",
+    "I really need help staying vegan",
+    "I think that i want to go vegan",
+    "I think i should go vegan",
+    "i will need help going vegan",
+    "i do need help going vegan",
+    "i definitely want to go vegan",
+    "i definately want to go vegan",
+    "i honestly would like to go vegan",
+    "i truly want to try going vegan",
+    "i wanna try becoming a vegan",
+    "i'm thinking about going vegan",
+    "i'm thinking of going vegan",
+    "im thinking about becoming vegan",
+    "i am considering being a vegan",
+    "i'm mulling over becoming vegan",
 ];
 
 var falsePositives = [
     "I do not want to go vegan",
 ];
 
-exports.testMatches = function(test) {
+exports.matches = function(test) {
     matches.forEach(function(match) {
         test.ok(filter.matches(match), "'" + match + "' should match");
     })
@@ -34,19 +54,19 @@ exports.falsePositives = function(test) {
     test.done();
 };
 
-exports.testRetweeted = function(test) {
+exports.retweeted = function(test) {
     var retweetedTweet = {retweeted: true, text: "", user: {description: "", name: "", screen_name: ""}};
     test.ok(!filter.matches(retweetedTweet), "Filter should not match retweeted tweets");
     test.done();
 }
 
-exports.testReply = function(test) {
+exports.reply = function(test) {
     var replyTweet = {retweeted: false, text: "@someone I want to go vegan", user: {description: "", name: "", screen_name: ""}};
     test.ok(!filter.matches(replyTweet), "Filter should not match @reply tweets");
     test.done();
 }
 
-exports.testExcludedTerms = function(test) {
+exports.excludedTerms = function(test) {
     var excludedBioTweet = {retweeted: false, text: "I want to go vegan", user: {description: "Something and excludeme", name: "", screen_name: ""}};
     var excludedNameTweet = {retweeted: false, text: "I want to go vegan", user: {description: "", name: "EXCLUDEME", screen_name: ""}};
     var excludedScreenNameTweet = {retweeted: false, text: "I want to go vegan", user: {description: "", name: "", screen_name: "excludeme"}};
