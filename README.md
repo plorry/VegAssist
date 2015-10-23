79e3180292cb1b01604b6e42230bfa14d536c97c
Ryan Liptak <squeek502@hotmail.com>
2015-10-22 17:36:54 -0700

    Refactor, fix some stuff, and add tests  * No changes in functionality, just refactored it to make it more robust/testable and added tests  * Fix #1  * Fix #2

--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 Twitter Bot that retweets people looking for support going vegan
 
 VegAssist tracks usage of the term "vegan" on twitter's public stream, and processes each item, looking for exact matches on an array of preset phrases. If there's a match, the bot will retweet.
+
+## Installing
+
+1. Clone this repository
+2. Install [node.js and npm](https://nodejs.org)
+3. Run `npm install` in the directory that you cloned this repository into
+4. Create a `settings.js` file with the format below, filling in your Twitter API credentials:
+5. Run `node .`
+
+#### `settings.js` format:
+```javascript
+module.exports = {
+  CREDS: {
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...'
+  }
+}
+```
+
+## Running the tests
+
+Simply run `npm test`
---
+++ test/test-filter.js
@@ -0,0 +1,54 @@
+var TweetFilter = require('../lib/filter.js')
+var filter = new TweetFilter('filters', ['excludeme'])
+
+var matches = [
+    'help me go vegan',
+    'help me become vegan',
+    'help me be vegan',
+    'i want to go vegan',
+    'i want to become vegan',
+    'i want to be vegan',
+    'i wanna go vegan',
+    'i wanna be vegan',
+];
+
+var falsePositives = [
+    "I do not want to go vegan",
+];
+
+exports.testMatches = function(test) {
+    matches.forEach(function(match) {
+        test.ok(filter.matches(match), "'" + match + "' should match");
+    })
+    test.done();
+};
+
+exports.falsePositives = function(test) {
+    falsePositives.forEach(function(falsePositive) {
+        test.ok(!filter.matches(falsePositive), "'" + falsePositive + "' should not match");
+    })
+    test.done();
+};
+
+exports.testRetweeted = function(test) {
+    var retweetedTweet = {retweeted: true, text: "", user: {description: "", name: "", screen_name: ""}};
+    test.ok(!filter.matches(retweetedTweet), "Filter should not match retweeted tweets");
+    test.done();
+}
+
+exports.testReply = function(test) {
+    var replyTweet = {retweeted: false, text: "@someone I want to go vegan", user: {description: "", name: "", screen_name: ""}};
+    test.ok(!filter.matches(replyTweet), "Filter should not match @reply tweets");
+    test.done();
+}
+
+exports.testExcludedTerms = function(test) {
+    var excludedBioTweet = {retweeted: false, text: "I want to go vegan", user: {description: "Something and excludeme", name: "", screen_name: ""}};
+    var excludedNameTweet = {retweeted: false, text: "I want to go vegan", user: {description: "", name: "EXCLUDEME", screen_name: ""}};
+    var excludedScreenNameTweet = {retweeted: false, text: "I want to go vegan", user: {description: "", name: "", screen_name: "excludeme"}};
+
+    test.ok(!filter.matches(excludedBioTweet), "Filter should not match tweets from users with excluded terms in their bio");
+    test.ok(!filter.matches(excludedNameTweet), "Filter should not match tweets from users with excluded terms in their name");
+    test.ok(!filter.matches(excludedScreenNameTweet), "Filter should not match tweets from users with excluded terms in their screen name");
+    test.done();
+}
