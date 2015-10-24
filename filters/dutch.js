6d6c48f6c98b0a3f33a0d149b129f91d49fee0be
joriskcf <github@kcf.me>
2015-10-24 14:42:37 +0200

    Create dutch.js

---
+++ filters/dutch.js
@@ -0,0 +1,8 @@
+module.exports = [
+    'help me veganist te worden',
+    'help me vegan te worden',
+    'ik wil veganist worden',
+    'ik wil vegan worden',
+    'ik wil veganist zijn',
+    'ik wil vegan zijn',
+]
