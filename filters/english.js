fcde417d1e1c12a5f9db853ae44b74f7fa9c26e0
squeek <squeek502@hotmail.com>
2015-10-24 20:19:26 -0700

    Exclude "I should be a vegan" as a false positive  * this phrasing more than likely isn't about going vegan long-term (e.g. I should be a vegan for Halloween)

--- filters/english.js
+++ filters/english.js
@@ -16,7 +16,8 @@ var robustRegex = function(regexStr, flags) {
 
 module.exports = [
     robustRegex( "help me (be( a)?|become( a)?|go) #?vegan" ),
-    robustRegex( "i (" + adverbsRegexSet + ")? ?(want to|wanna|would like to|should) (be( a)?|become( a)?|go) #?vegan" ),
+    robustRegex( "i (" + adverbsRegexSet + ")? ?(want to|wanna|would like to) (be( a)?|become( a)?|go) #?vegan" ),
+    robustRegex( "i (" + adverbsRegexSet + ")? ?(should) (go|be) #?vegan" ),
     robustRegex( "i (" + adverbsRegexSet + ")? ?(will|do)? ?(need|want) help (going|becoming( a)?|being( a)?|staying( a)?) #?vegan" ),
     robustRegex( "i (" + adverbsRegexSet + ")? ?(want to|wanna|would like to|should) (try) (going|becoming( a)?|being( a)?) #?vegan" ),
     robustRegex( "i('| a)?m (considering|thinking (about|of)|mulling over) (going|becoming( a)?|being( a)?) #?vegan" ),
--- test/test-filter.js
+++ test/test-filter.js
@@ -41,6 +41,7 @@ var matches = [
 
 var falsePositives = [
     "I do not want to go vegan",
+    "I should be a vegan", // this phrasing more than likely isn't about going vegan long-term (e.g. I should be a vegan for Halloween)
 ];
 
 exports.matches = function(test) {
