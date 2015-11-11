07cd9927357da2329e4ad4caa096e2915575536f
Filipe Dobreira <dobreira@gmail.com>
2015-11-11 20:54:01 +0000

    Add Portuguese (pt-PT) filters

---
+++ filters/portuguese.js
@@ -0,0 +1,5 @@
+module.exports = [
+    /quero (tornar\-me|ser) vegan/gi,
+    /como (me torno|me posso tornar|posso tornar\-me) vegan/gi,
+    /gosta(va|ria) de (ser|me tornar|tornar\-me) vegan/gi,
+];
