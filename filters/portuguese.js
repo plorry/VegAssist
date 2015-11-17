fd74bb9f053ff3d310114d5716c44a121e0044a6
Ryan Liptak <squeek502@hotmail.com>
2015-11-17 14:06:56 -0800

    Fix Portuguese negated false positives and add tests

--- filters/portuguese.js
+++ filters/portuguese.js
@@ -1,10 +1,13 @@
 var util = require("../lib/util");
 var track = util.track;
+var excludeBefore = util.excludeBefore;
 
 track("vegan");
 
+var negations = ["não", "nao", "no", "not", "non", "nay", "nope", "nenhum(a)?"]
+
 module.exports = [
-    /quero (tornar\-me|ser) vegan/gi,
+    excludeBefore(/quero (tornar\-me|ser) vegan/gi, negations),
     /como (me torno|me posso tornar|posso tornar\-me) vegan/gi,
-    /gosta(va|ria) de (ser|me tornar|tornar\-me) vegan/gi,
+    excludeBefore(/gosta(va|ria) de (ser|me tornar|tornar\-me) vegan/gi, negations),
 ];
