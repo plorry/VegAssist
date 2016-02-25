7b9e576503cd2a8ed4310f3b1dd582273f3db13e
Andrew Gardner <timebot@gmail.com>
2016-02-24 23:06:25 -0500

    lower-case all matches in french.js

--- filters/french.js
+++ filters/french.js
@@ -5,10 +5,10 @@ var track = util.track;
 track("végétalien", "vegan");
 
 module.exports = [
-    regex( "Je deviens (végétalien|vegan)" ),
+    regex( "je deviens (végétalien|vegan)" ),
     regex( "je vais (devenir|être) (végétalien|vegan)" ),
     regex( "je veux( bien)? (devenir|être) (végétalien|vegan)" ),
     regex( "je veux (devenir|être)( totalement)? (végétalien|vegan)" ),
     regex( "je suis( vraiment)? motivé à (devenir|être) (végétalien|vegan)" ),
-    regex( "J'aimerais (devenir|être) (végétalien|vegan)" )
+    regex( "j'aimerais (devenir|être) (végétalien|vegan)" )
 ]
